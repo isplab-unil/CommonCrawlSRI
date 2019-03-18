200f7472790860640c66c598da7ead68a28a3f4f
Bertil Chapuis <bchapuis@gmail.com>
2019-03-18 17:41:46 +0100

    move top1m to input dir

--- report.py
+++ report.py
@@ -1,6 +1,6 @@
 from pyspark.shell import sqlContext
 
-sqlContext.read.csv('top-1m.csv').registerTempTable('top1m')
+sqlContext.read.csv('input/top-1m.csv').registerTempTable('top1m')
 
 sqlContext.read.parquet("output/*.parquet").registerTempTable("cc")
 
