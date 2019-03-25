229cf8d265a929d33f00c59e8f2fcf8ebe35f6bc
Bertil Chapuis <bchapuis@gmail.com>
2019-03-25 10:44:13 +0100

    refactoring

--- tests/benchmarks.py
+++ tests/benchmarks.py
@@ -14,7 +14,7 @@ import time
 
 from warcio import ArchiveIterator
 
-from wet import Wet
+from downloadswet import DownloadsWet
 
 def parse_arguments():
     """ Returns the parsed arguments from the command line """
@@ -39,7 +39,7 @@ def process_warc(input, limit):
     keywords_count = 0
 
     # mock the behaviour of the processor
-    processor = Wet()
+    processor = DownloadsWet()
     with open(input) as warcs:
         for warc in warcs.read().splitlines():
             with open(warc, 'rb') as stream:
