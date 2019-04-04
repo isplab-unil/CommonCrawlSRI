5a155670462ec2b5b51156faf1251840ab9a3382
Bertil Chapuis <bchapuis@gmail.com>
2019-04-04 11:02:23 +0200

    minor fixes

--- reports/download.py
+++ reports/download.py
@@ -49,7 +49,7 @@ sqlContext.sql("""
 SELECT 
     round(100 * count(*) / (SELECT count(*) FROM cc), 2) AS percentage 
 FROM cc 
-WHERE size(keywords) > 0
+WHERE has_keyword
 """).show(20, False)
 
 # Percentage of web pages containing checksums in their content
@@ -57,7 +57,7 @@ sqlContext.sql("""
 SELECT 
     round(100 * count(*) / (SELECT count(*) FROM cc), 2) AS percentage 
 FROM cc 
-WHERE size(checksums) > 0
+WHERE has_checksum
 """).show(20, False)
 
 
@@ -65,7 +65,14 @@ sqlContext.sql("""
 SELECT COUNT(*)
 FROM cc JOIN top1m ON ( substring_index(substring_index(url, '/', 3), '/', -1) = _c1)
 WHERE  has_keyword AND has_checksum AND url LIKE '%download%'  
-""").show(1000, False)
+""").show(20, False)
+
+
+sqlContext.sql("""
+SELECT COUNT(*)
+FROM cc JOIN top1m ON ( substring_index(substring_index(url, '/', 3), '/', -1) = _c1)
+WHERE has_checksum = true AND has_keyword = true
+""").show(20, False)
 
 
 # ---------------------------
@@ -105,17 +112,19 @@ render('776426eeb4f0752fa3f9750ddaf29f1364bccc84',
        ['7706292fe4b2f7eb988a7b688c29cbe9c8e86f7f51c759c5aab9fc176e695f44',
         '4c50f8657a6f19e73468bac563c1804e112c54c1f700d24803cacc22d080d08b'])
 
+#.foreach(lambda r: render(r.id, r.url, r.checksums))
+
 sqlContext.sql("""
 SELECT sha1(url) as id, url, checksums
 FROM cc JOIN top1m ON ( substring_index(substring_index(url, '/', 3), '/', -1) = _c1)
 WHERE has_checksum = true AND has_keyword = true AND url LIKE '%download%'
 ORDER BY CAST(_c0 as INT)
 LIMIT 10
-""").foreach(lambda r: render(r.id, r.url, r.checksums))
+""").show(20, False)
 
 
 sqlContext.sql("""
-SELECT _c0, url
+SELECT sha1(url) as id, _c0, url
 FROM cc JOIN top1m ON ( substring_index(substring_index(url, '/', 3), '/', -1) = _c1)
 WHERE has_checksum = true AND has_keyword = true
 ORDER BY CAST(_c0 as INT)
@@ -126,7 +135,7 @@ SELECT sha1(url) as id, url, checksums
 FROM cc JOIN top1m ON ( substring_index(substring_index(url, '/', 3), '/', -1) = _c1)
 WHERE has_checksum = true AND has_keyword = true AND url LIKE '%download%'
 ORDER BY CAST(_c0 as INT)
-""").foreach(lambda r: render(r.id, r.url, r.checksums))
+""").show(20, False)
 
 
 sqlContext.sql("""
@@ -135,8 +144,4 @@ FROM cc JOIN top1m ON ( substring_index(substring_index(url, '/', 3), '/', -1) =
 WHERE has_checksum = true AND has_keyword = true
 """).show(20, False)
 
-sqlContext.sql("""
-SELECT count(*)
-FROM cc 
-""").show(20, False)
 
