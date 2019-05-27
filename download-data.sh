f514a6398b80a7a3bb7ea32552a6c1990dc4c320
Bertil Chapuis <bchapuis@gmail.com>
2019-05-27 10:36:22 +0200

    Disable the creation of the sample

--- download-data.sh
+++ download-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CRAWL=CC-MAIN-2017-13
+CRAWL=CC-MAIN-2016-36
 
 BASE_URL=https://commoncrawl.s3.amazonaws.com
 
@@ -24,14 +24,14 @@ for data_type in warc wat wet; do
 	wget --timestamping $BASE_URL/$listing
 	cd -
 
-	file=$(gzip -dc $listing | head -1)
-	mkdir -p $(dirname $file)
-	cd $(dirname $file)
-	wget --timestamping $BASE_URL/$file
-	cd -
+	#file=$(gzip -dc $listing | head -1)
+	#mkdir -p $(dirname $file)
+	#cd $(dirname $file)
+	#wget --timestamping $BASE_URL/$file
+	#cd -
 
-	echo file:$PWD/$file >>input/test_${data_type}.txt
-	gzip -dc $listing | sed 's@^@s3://commoncrawl/@' \
-		>input/all_${data_type}_$CRAWL.txt
+	#echo file:$PWD/$file >>input/test_${data_type}.txt
+	#gzip -dc $listing | sed 's@^@s3://commoncrawl/@' \
+	#	>input/all_${data_type}_$CRAWL.txt
 
 done
